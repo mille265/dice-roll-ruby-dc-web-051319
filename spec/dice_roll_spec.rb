29442d9d374f6bf2925bd6479dec656f7de18eff
meghan miller <mille265@gmail.com>
2019-05-07 17:06:52 +0000

    Done.

--- spec/dice_roll_spec.rb
+++ spec/dice_roll_spec.rb
@@ -1,26 +1,7 @@
-require_relative '../dice_roll'
+# Create method `roll` that returns a random number between 1 and 6
 
-describe "#roll" do
+# Feel free to google "how to generate a random number in ruby"
 
-  it "should return an Integer" do
-    expect(roll).to be_a(Integer)
-  end
-
-  it "should return a number greater than 0" do
-    expect(roll).to be > 0
-  end
-
-  it "should return a number less than 7" do
-    expect(roll).to be < 7
-  end
-
-  it "should return a random value" do
-    rolls = []
-    100.times do
-      rolls << roll
-    end
-    expect(rolls).to include(1, 2, 3, 4, 5, 6)
-    expect(rolls).not_to include(0, 7)
-  end
-
+def roll
+  rand(1..6)
 end
